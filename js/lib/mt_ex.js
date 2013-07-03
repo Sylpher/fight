a8caa05db33557372ae55e6801c775aa0669fd46
Ruben <sylpher@yahoo.com>
2013-07-03 15:38:22 -0700

    Update mt_ex.js

--- js/lib/mt_ex.js
+++ js/lib/mt_ex.js
@@ -8,3 +8,14 @@ MersenneTwister19937.prototype.init_by_string = function(seed) {
 	}
 	this.init_by_array(s, s.length);
 };
+
+// Simple dice roller. Returns each die, total and average.
+MersenneTwister19937.prototype.dice = function(sides, num) {
+	var d = { dice: new Array(num), total: 0, average:0 };
+	for(var c=0; c<num; c++) {
+		d.dice[c] = (this.genrand_int32() % sides) + 1; // +1 zero based
+		d.total += d.dice[c];
+	}
+	d.average = d.total / num;
+	return d;
+};
